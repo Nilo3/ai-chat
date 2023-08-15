d02704d84c1254347ffaebdde232d694de3eee07
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2023-08-15 20:12:44 -0300

    sidebar work in pc and mobile devices

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,28 +1,32 @@
 "use client";
 
 import Link from "next/link";
-import { Menu, Sparkles } from "../node_modules/lucide-react/";
-import { Poppins } from "next/font/google"
+import { Sparkles } from "../node_modules/lucide-react/";
+import { Poppins } from "next/font/google";
 import { UserButton } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./mode-toggle";
+import { MobileSidebar } from "./mobile-sidebar";
 
 const font = Poppins({
   weight: "600",
-  subsets: ["latin"]
-})
+  subsets: ["latin"],
+});
 
 export const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="block md:hidden" />
+        <MobileSidebar />
         <Link href="/">
-          <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary",
-            font.className
-          )}>
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
             Ai-Chat
           </h1>
         </Link>
@@ -32,9 +36,8 @@ export const Navbar = () => {
           Upgrade
           <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
         </Button>
-        <ModeToggle /> 
+        <ModeToggle />
         <UserButton />
-        
       </div>
     </div>
   );
